979a41ce5c9787ea67fa9597339e2398d6bda9c5
Dan Kesov <48454102+SimpleOne1@users.noreply.github.com>
2026-09-13 10:46:44 +0200

    Use App Router locale paths

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,8 @@
 // next.config.ts
 import type { NextConfig } from "next";
-import nextI18NextConfig from "./src/i18n/next-i18next.config.js";
-
-const { i18n } = nextI18NextConfig;
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  i18n: {
-    defaultLocale: i18n.defaultLocale,
-    locales: i18n.locales,
-  },
 };
 
 export default nextConfig;
